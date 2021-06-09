75b505e0e8c78d1b51c90462939363499c621ad6
Tim Seidel <account@timseidel.de>
2021-06-09 13:05:49 +0200

    Updated vue config

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,5 @@
 module.exports = {
-  configureWebpack: {
-    plugins: []
-  },
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:8080',
-        ws: true,
-        changeOrigin: true
-      },
-      '^/swagger': {
-        target: 'http://localhost:8080',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  }
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
