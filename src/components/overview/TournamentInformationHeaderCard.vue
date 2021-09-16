a7004480aba5d5c32ad7e32bc808ad03c989d0b1
Tim Seidel <account@timseidel.de>
2021-09-16 12:24:56 +0200

    Moved delete dialog, added sha256

--- src/components/overview/TournamentInformationHeaderCard.vue
+++ src/components/overview/TournamentInformationHeaderCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="fill-height" v-if="!tournament">
-    <v-card-title> Turnierübersicht </v-card-title>
+    <v-card-title> Turnierübersicht</v-card-title>
     <v-card-text>
       Aktuell stehen keine Turnierinformationen bereit.
     </v-card-text>
@@ -10,12 +10,12 @@
     <v-card-title class="mb-2">
       {{ tournament.name }}
       <v-spacer></v-spacer>
-      <v-btn class="ml-2" color="error" @click="onDeleteClicked">
+      <v-btn class="ml-2" color="error" @click="dialogDelete.isVisible = true">
         <v-icon>mdi-delete</v-icon>
-        LÖschen
+        Löschen
       </v-btn>
       <v-btn class="ml-2" color="primary" @click="onSelect"
-        >Gruppenphase
+      >Gruppenphase
         <v-icon right>mdi-send</v-icon>
       </v-btn>
     </v-card-title>
@@ -23,12 +23,60 @@
       Hier siehst Du die Übersicht zum aktuellen Turnierstand. Du kannst hier
       Einstellungen vornehmen, oder die Gruppenphase direkt starten.
     </v-card-subtitle>
+
+    <v-dialog v-model="dialogDelete.isVisible" max-width="600">
+      <v-card>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col class="mt-2 pl-0" cols="12">
+                <h1>Turnier wirklich löschen?</h1>
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-alert type="warning"
+              >Das Turnier kann dann nicht wiederhergestellt werden, außer du hast eine Speicherdatei!
+              </v-alert>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <a class="save-link" :download="fileName" :href="fileUrl">
+                Kopie Speichern</a>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="mt-2 pb-0" cols="12">
+              <p class="mb-2">
+                Gib zur Sicherheit den Admin-Key zum Löschen ein.
+              </p>
+            </v-col>
+            <v-col class="pt-0 pb-0" cols="12">
+              <v-text-field
+                  outlined
+                  label="delete_key"
+                  type="password"
+                  v-model="dialogDelete.deleteKey"/>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn color="primary" text @click="dialogDelete.isVisible = false">Abbrechen</v-btn>
+          <v-btn color="error" text @click="onDeleteTournament">Löschen</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
 import * as moment from "moment";
-import { deleteTournament } from "../../services/TournamentService";
+import {deleteTournament} from "../../services/TournamentService";
+import {sha256} from "js-sha256";
+import {convertJsonToTournament} from "../../util/Converter";
 
 export default {
   props: {
@@ -37,7 +85,12 @@ export default {
       default: undefined,
     },
   },
-  data: () => ({}),
+  data: () => ({
+    dialogDelete: {
+      isVisible: false,
+      deleteKey: "",
+    }
+  }),
   methods: {
     formatDate(date) {
       if (!date) return "Unbekannt";
@@ -46,14 +99,50 @@ export default {
     onSelect() {
       this.$store.state.tournament.current = this.tournament;
       localStorage.setItem("currentTournamentId", this.tournament?.id);
-      this.$router.push({ name: "GroupStage" });
+      this.$router.push({name: "GroupStage"});
     },
-    async onDeleteClicked() {
-      deleteTournament(this.tournament.id);
+    async onDeleteTournament() {
+      const salt = "Bierpong: "
+      const compareKey = "c4b6dbb0456345c48e71aa8735d29985699dccce0fe6460f4203c80b19207e6a"
+      const enteredKey = sha256(salt + this.dialogDelete.deleteKey)
 
-      this.$router.back();
+      if (enteredKey === compareKey) {
+        this.dialogDelete.isVisible = false;
+        deleteTournament(this.tournament.id);
+        this.$router.back();
+      }
     },
-  },
-  computed: {},
+    fileUrl() {
+      if (!this.tournament) {
+        return "";
+      }
+
+      const json = convertJsonToTournament(this.tournament);
+      const file = new Blob([JSON.stringify(json)], {type: "text/json"});
+      return URL.createObjectURL(file);
+    },
+    fileName() {
+      return this.appTitle + ".json"
+    }
+  }
 };
 </script>
+
+<style scoped>
+.save-link {
+  border: thin solid steelblue;
+  border-radius: 4px;
+  align-items: center;
+  font-weight: 500;
+  letter-spacing: 0.089em;
+  display: inline-flex;
+  text-transform: uppercase;
+  text-decoration: none;
+  height: 36px;
+  padding: 0 16px;
+}
+
+.save-link:hover {
+  background-color: lightblue;
+}
+</style>
--- src/components/toolbar/TournamentAppBar.vue
+++ src/components/toolbar/TournamentAppBar.vue
@@ -22,10 +22,10 @@
       </v-btn>
 
       <v-btn
-        @click="dialogEnd.isVisible = true"
-        class="ml-1 mr-1"
-        color="error"
-        icon
+          @click="onEndTournament"
+          class="ml-1 mr-1"
+          color="error"
+          icon
       >
         <v-icon>mdi-close</v-icon>
       </v-btn>
@@ -33,12 +33,13 @@
       <v-dialog v-model="dialogAdd" persistent max-width="600">
         <v-card>
           <v-card-text>
-            <add-to-tournament :groups="visibleGroups" />
+            <add-to-tournament :groups="visibleGroups"/>
           </v-card-text>
           <v-card-actions>
-            <v-spacer />
+            <v-spacer/>
             <v-btn color="blue darken-1" text @click="dialogAdd = false"
-              >Schließen</v-btn
+            >Schließen
+            </v-btn
             >
           </v-card-actions>
         </v-card>
@@ -47,12 +48,13 @@
       <v-dialog v-model="dialogSettings" persistent max-width="600">
         <v-card>
           <v-card-text>
-            <layout-options :groups="groups" />
+            <layout-options :groups="groups"/>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="blue darken-1" text @click="dialogSettings = false"
-              >Schließen</v-btn
+            >Schließen
+            </v-btn
             >
           </v-card-actions>
         </v-card>
@@ -67,69 +69,19 @@
                   <h1>Turnier speichern</h1>
                 </v-col>
                 <v-col cols="12">
-                  <a class="save-link" download="tournament.json" :href="fileUrl"
-                    >Datei herunterladen</a
+                  <a class="save-link" :download="fileName" :href="fileUrl"
+                  >Datei herunterladen</a
                   >
                 </v-col>
               </v-row>
             </v-container>
           </v-card-text>
           <v-card-actions>
-            <v-spacer />
+            <v-spacer/>
             <v-btn color="primary" text @click="dialogSave = false"
-              >Schließen</v-btn
+            >Schließen
+            </v-btn
             >
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
-      <v-dialog v-model="dialogEnd.isVisible" max-width="400">
-        <v-card>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col class="mt-2 pl-0" cols="12">
-                  <h1>Turnier speichern</h1>
-                </v-col>
-              </v-row>
-            </v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-alert type="warning"
-                  >Wenn Du jetzt beendest gehen ungespeicherte Änderungen
-                  verloren.</v-alert
-                >
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="12">
-                <a class="save-link" download="tournament.json" :href="fileUrl">
-                  Kopie Speichern</a
-                >
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col class="mt-2 pb-0" cols="12">
-                <p class="mb-2">
-                  Gib zur Sicherheit den Admin-Key zum Löschen ein.
-                </p>
-              </v-col>
-              <v-col class="pt-0 pb-0" cols="12">
-                <v-text-field
-                  outlined
-                  label="delete_key"
-                  type="password"
-                  v-model="dialogEnd.deleteKey"
-                />
-              </v-col>
-            </v-row>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn color="primary" text @click="dialogEnd = false"
-              >Abbrechen</v-btn
-            >
-            <v-btn color="error" text @click="onEndTournament">Beenden</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -139,14 +91,9 @@
 
 <script>
 import Vue from "vue";
-import { v4 as uuid } from "uuid";
 
 import AddToTournament from "@/components/dialog/AddToTournament";
 import LayoutOptions from "@/components/dialog/LayoutOptions";
-
-import Team from "@/model/Team";
-import Game from "@/model/Game";
-import {convertTournamentToJson} from "@/model/util/Converter"
 
 export default Vue.extend({
   components: {
@@ -156,10 +103,6 @@ export default Vue.extend({
   data: () => {
     return {
       dialogAdd: false,
-      dialogEnd: {
-        isVisible: false,
-        deleteKey: "",
-      },
       dialogSave: false,
       dialogSettings: false,
       href: "",
@@ -172,120 +115,17 @@ export default Vue.extend({
     appTitle() {
       return this.tournament?.name ?? "Nicht geladen";
     },
-
     visibleGroups() {
       return this.$store.getters.visibleGroups ?? [];
     },
-
-    fileUrl() {
-      if (!this.tournament) {
-        return "";
-      }
-
-      const json = convertTournamentToJson(this.tournament);
-      const file = new Blob([JSON.stringify(json)], { type: "text/json" });
-      return URL.createObjectURL(file);
-    },
-
-    addDialogSelectedGroup() {
-      return this.groups.find((g) => g.name === this.dialogAdd.group);
-    },
-
-    addDialogGroups() {
-      const groups = [];
-      if (this.groups) {
-        this.groups.forEach((g) => {
-          groups.push(g.name);
-        });
-      }
-      return groups;
-    },
-
-    addDialogSelectedTeams() {
-      const teams = [];
-      if (this.addDialogSelectedGroup) {
-        this.addDialogSelectedGroup.teams.forEach((t) => {
-          teams.push(t.name);
-        });
-      }
-      return teams;
-    },
-
     groups() {
       return this.tournament?.groups ?? [];
     },
-  },
-  methods: {
     onEndTournament() {
-      if (this.dialogEnd.deleteKey === "Ich bin nicht betrunken!") {
-        this.dialog = false;
-        localStorage.removeItem("currentTournamentId");
-        this.$store.state.tournament.current = null;
-        this.$router.push("/");
-      }
-    },
-
-    resetAddDialog() {
-      this.dialogAdd.team1 = "";
-      this.dialogAdd.team2 = "";
-      this.dialogAdd.teamName = "";
-      this.dialogAdd.error = "";
-    },
-
-    saveTeam() {
-      const grp = this.addDialogSelectedGroup;
-      if (grp && !grp.hasStarted()) {
-        grp.teams.push(new Team(uuid(), this.dialogAdd.teamName));
-        grp.createGames();
-        this.resetAddDialog();
-      } else {
-        alert(
-          "Angefangene Gruppen können leider keine weiteren Teams hinzufügen."
-        );
-      }
-    },
-
-    saveGame() {
-      const grp = this.addDialogSelectedGroup;
-      let error = false;
-      if (grp) {
-        const team1 = grp.teams.find((t) => t.name === this.dialogAdd.team1);
-        const team2 = grp.teams.find((t) => t.name === this.dialogAdd.team2);
-        if (team1 && team2) {
-          grp.games.push(new Game(uuid(), team1, team2));
-          grp.calculateStandings();
-        } else {
-          error = true;
-        }
-      } else {
-        error = true;
-      }
-
-      if (error) {
-        alert("Fehler beim hinzufügen des Teams. Objekt nicht erkannt");
-      } else {
-        this.resetAddDialog();
-      }
-    },
-  },
+      localStorage.removeItem("currentTournamentId");
+      this.$store.state.tournament.current = null;
+      this.$router.push("/");
+    }
+  }
 });
 </script>
-
-<style scoped>
-.save-link {
-  border: thin solid steelblue;
-  border-radius: 4px;
-  align-items: center;
-  font-weight: 500;
-  letter-spacing: 0.089em;
-  display: inline-flex;
-  text-transform: uppercase;
-  text-decoration: none;
-  height: 36px;
-  padding: 0 16px;
-}
-
-.save-link:hover {
-  background-color: lightblue;
-}
-</style>
