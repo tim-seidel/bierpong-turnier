938a671dbe8e9c99362fa4f61c66b3983ea6afa2
Tim Seidel <account@timseidel.de>
2021-10-25 18:09:25 +0200

    Margin adjustment & formatting

--- src/components/TeamChip.vue
+++ src/components/TeamChip.vue
@@ -1,16 +1,16 @@
 <template>
   <h4 v-if="simple">{{ name }}</h4>
-  <v-chip  v-else-if="!right" :color="color" outlined pill>
+  <v-chip v-else-if="!right" :color="color" outlined pill>
     <v-avatar :color="color" left>
       <h3 class="white--text">{{ initials }}</h3>
       <!--<v-icon color="white">mdi-account-multiple</v-icon>-->
     </v-avatar>
-    <h3 class="primary--text ml-2 mr-2">
+    <h3 class="primary--text ml-1">
       {{ name ? name : "?" }}
     </h3>
   </v-chip>
-  <v-chip  v-else :color="color" outlined pill>
-    <h3 class="primary--text ml-2 mr-2">
+  <v-chip v-else :color="color" outlined pill>
+    <h3 class="primary--text mr-1">
       {{ name ? name : "?" }}
     </h3>
     <v-avatar :color="color" right>
@@ -48,17 +48,17 @@ export default {
   computed: {
     initials() {
       const parts = this.name
-        .slice()
-        .replace("+", " ")
-        .split(" ")
-        .map((s) => s.trim())
-        .filter(Boolean);
+          .slice()
+          .replace("+", " ")
+          .split(" ")
+          .map((s) => s.trim())
+          .filter(Boolean);
       if (!parts || parts.length === 0) return "?";
       else if (parts.length === 1) {
         return parts[0].toUpperCase().charAt(0);
       } else {
         return (
-          parts[0].toUpperCase().charAt(0) + parts[1].toUpperCase().charAt(0)
+            parts[0].toUpperCase().charAt(0) + parts[1].toUpperCase().charAt(0)
         );
       }
     },
